5289f312cb3f7bbc1f50933220e6ff793bcb68f1
Shivam-website <shivamsah1222@gmail.com>
2025-11-02 14:27:49 +0545

    Added loops chapter ps

--- learningPython/CHapter 7/Chapter 7 -PS/08_problem8.py
+++ learningPython/CHapter 7/Chapter 7 -PS/08_problem8.py
@@ -6,7 +6,6 @@ For n = 3
 '''
 n =  int(input("Enter a num :"))#n=5
 for i in range(1,n+1):
-    print(" "*(n-i),end="")
-    print("*"*(2*i-1),end="")
+    print("*"*i,end="")
     print("")
      
---
+++ learningPython/CHapter 7/Chapter 7 -PS/10_problem10.py
@@ -0,0 +1,4 @@
+n = int(input("Enter a num :"))
+for i in range(1,11):
+    print(f"{n} x {11-i} = {n*(11-i)}")
+   
