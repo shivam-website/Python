692743d6fbafa7182c5a07a9b36ba9c7cd997800
Shivam-website <shivamsah1222@gmail.com>
2025-11-04 17:53:35 +0545

    ADDED Project 1

---
+++ learningPython/Project 1/app.py
@@ -0,0 +1,46 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+import random
+computer = random.choice([1,-1,0])
+youstr = input("Enter your choice:")
+youDict = {
+    "s":1,
+    "w":-1,
+    "g":0
+}
+reverseDict = {
+    1:"snake",
+    -1:"water",
+    0:"gun"
+}
+
+you=youDict[youstr]
+
+print(f"you choosed {reverseDict[you]}")
+print(f"computer choosed {reverseDict[computer]}")
+if(computer==you):
+    print("Draw")
+else:
+    if(computer==-1 and you ==1):
+        print("You won")
+
+    elif(computer==-1 and you==0):
+        print("You lost")
+
+    elif(computer==1 and you==-1):
+        print("You lost")
+        
+    elif(computer==1 and you==0):
+        print("You won")
+
+    elif(computer==0 and you==-1):
+        print("You win")
+
+    elif(computer==0 and you==1):
+        print("You lost")
+    else:
+        print("Draw")
+
